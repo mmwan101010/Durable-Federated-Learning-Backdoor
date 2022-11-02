90295a24333e51f8448a92df79854ec4681d7a9f
Xu Weida <imomoe233@outlook.com>
2022-11-02 17:39:15 +0800

    调参

--- FL_Backdoor_CV/run_backdoor_cv_task.sh
+++ FL_Backdoor_CV/run_backdoor_cv_task.sh
@@ -5,7 +5,7 @@ finished baseline - nohup python main_training.py --run_slurm 0 --GPU_id 0  --st
 
 finished nohup python main_training.py --run_slurm 0 --GPU_id 0  --start_epoch 1801 --is_poison True --defense True --s_norm 0.2 --attack_num 250 --gradmask_ratio 0.99 --poison_lr 0.02 --aggregate_all_layer 1 --edge_case 0
 # 结果每轮准确率为0% 考虑调整s_norm / Lr / attack_num
-# 将poison_lr 从 0.02 -> 0.2     
+# 将poison_lr 从 0.02 -> 0.05  &&  s_norm 0.2 -> 0.3  && batch_size 64 -> 512     
 'start at 1841 epoch，1840 model from <Backdoor_saved_models_update1_noniid_0.9_cifar10_EC0_EE3801>-<Backdoor_model_cifar10_resnet_maskRatio0.99_Snorm_0.2_checkpoint_model_epoch_1840>'
 'models save at <Backdoor_saved_models_update1_noniid_0.9_cifar10_EC0_EE3841> and <Backdoor_saved_models_update1_noniid_EC0_cifar10_Neurotoxin_GradMaskRation0.99_EE3841>'
 '同理 后门的准确率和损失也应该是分为两块存储在不同的文件夹下的，注意判别参数，在试验结束后汇总一下，统一汇总到EE3801中并做好标记，因为EE3841是因为开始是从41开始所以是41，故我们直接合并至EE3801中即可'
