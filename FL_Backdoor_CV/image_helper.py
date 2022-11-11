9a3d8c71f149c28d5f5b0edab099d445b44a6e60
daer <imomoe233@outlook.com>
2022-11-12 02:41:13 +0800

    update

--- FL_Backdoor_CV/image_helper.py
+++ FL_Backdoor_CV/image_helper.py
@@ -30,18 +30,8 @@ np.random.seed(0)
 import torch
 
 def get_poison_cifar10():
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_1', 'rb') as train_1:
+    with open('X:\Directory\code\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_1', 'rb') as train_1:
         poison_data1 = pickle.load(train_1)
-<<<<<<< Updated upstream
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_2', 'rb') as train_2:
-        poison_data2 = pickle.load(train_2)
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_3', 'rb') as train_3:
-        poison_data3 = pickle.load(train_3)
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_4', 'rb') as train_4:
-        poison_data4 = pickle.load(train_4)
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_5', 'rb') as train_5:
-=======
-<<<<<<< HEAD
     with open('X:\Directory\code\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_2', 'rb') as train_2:
         poison_data2 = pickle.load(train_2)
     with open('X:\Directory\code\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_3', 'rb') as train_3:
@@ -49,16 +39,6 @@ def get_poison_cifar10():
     with open('X:\Directory\code\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_4', 'rb') as train_4:
         poison_data4 = pickle.load(train_4)
     with open('X:\Directory\code\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_5', 'rb') as train_5:
-=======
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_2', 'rb') as train_2:
-        poison_data2 = pickle.load(train_2)
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_3', 'rb') as train_3:
-        poison_data3 = pickle.load(train_3)
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_4', 'rb') as train_4:
-        poison_data4 = pickle.load(train_4)
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_5', 'rb') as train_5:
->>>>>>> main
->>>>>>> Stashed changes
         poison_data5 = pickle.load(train_5)
 
     x1 = poison_data1.get('data').reshape(10000, 32, 32, 3)
@@ -78,9 +58,6 @@ def get_poison_cifar10():
 def get_poison_cifar10_train_label():    
     with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_1', 'rb') as train_1:
         poison_data1 = pickle.load(train_1)
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
     with open('X:\Directory\code\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_2', 'rb') as train_2:
         poison_data2 = pickle.load(train_2)
     with open('X:\Directory\code\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_3', 'rb') as train_3:
@@ -88,19 +65,7 @@ def get_poison_cifar10_train_label():
     with open('X:\Directory\code\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_4', 'rb') as train_4:
         poison_data4 = pickle.load(train_4)
     with open('X:\Directory\code\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_5', 'rb') as train_5:
-=======
->>>>>>> Stashed changes
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_2', 'rb') as train_2:
-        poison_data2 = pickle.load(train_2)
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_3', 'rb') as train_3:
-        poison_data3 = pickle.load(train_3)
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_4', 'rb') as train_4:
-        poison_data4 = pickle.load(train_4)
-    with open('D:\code\code_xwd\Durable-Federated-Learning-Backdoor\FL_Backdoor_CV\data\poison_cifar10\data_batch_5', 'rb') as train_5:
-<<<<<<< Updated upstream
-=======
->>>>>>> main
->>>>>>> Stashed changes
+
         poison_data5 = pickle.load(train_5)
 
     x1 = poison_data1.get('labels')
